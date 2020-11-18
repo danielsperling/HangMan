d37a9781fd056c40d0f74974e83884642b0ab4c9
Daniel Sperling <daniel_sperling@yahoo.com>
2020-11-18 22:53:22 +0200

    added notification same letter used

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
 
 // I want to have a list of words 
-const wordsArray = ['hello', 'goodbye', 'test', 'two words'];
+const wordsArray = ['hello', 'goodbye', 'test'];
 
 // Random words from the list should be chosen 
 const randomWordEl = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 const words = document.querySelector('.words');
 const wrongLetters = document.querySelector('.letters');
 const bodyPart = document.querySelectorAll('.bodyParts');
+const popup = document.querySelector('.notification');
+
+
+console.log(popup.innerHTML);
+
 
 
 
@@ -20,6 +25,12 @@ function displayWord(){
         letter => `<span class="letter"> 
             ${correctLetter.includes(letter) ? letter : '' }
         </span>`).join('')}`;
+
+        const correctWord = words.innerText.replace(/\n/g, '')
+        console.log(correctWord);
+        if(correctLetter === correctWord){
+            document.body.style.background = "#222";
+        }
 }
 
 
@@ -37,29 +48,50 @@ function displayWrongLetter(){
                 part.style.display = 'none';
             }
         });
+        
 
 }
 
 
-window.addEventListener('keydown', letter => {
-    console.log(letter.key)
-    console.log(randomWordEl)
+function showNotification() {
+    popup.classList.add('show');
+    
+    setTimeout(() => {
+        popup.classList.remove('show');
 
-        if(randomWordEl.includes(letter.key)){
-            if(!correctLetter.includes(letter.key))
-            correctLetter.push(letter.key);
-            console.log(correctLetter);
+    }, 2000);
+    
+}
+
+
+window.addEventListener('keydown', letter => {
+    if(letter.keyCode >= 65 && letter.keyCode <= 90){
+        const lettr = letter.key
+    
+
+        if(randomWordEl.includes(lettr)){
+            if(!correctLetter.includes(lettr))
+            correctLetter.push(lettr);
             displayWord();
 
-        }else if(!randomWordEl.includes(letter.key)){
-            if(!wrongLetter.includes(letter.key)){
-                wrongLetter.push(letter.key);
+        }else if(!randomWordEl.includes(lettr)){
+            if(!wrongLetter.includes(lettr)){
+                wrongLetter.push(lettr);
                 console.log(wrongLetter);
                 displayWrongLetter();
+            }else {
+                showNotification();
             }
         }
+    }
     });
 
+
+
+ 
+
+
+    displayWord()
 
 // I want when someone types a wrong letter to make the body parts appear
 
